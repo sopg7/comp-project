65625384aad1a8402d65cd7bd8c28743e39748a1
Alice <ajiang7585@gmail.com>
2025-10-17 02:07:45 +0000

    added word_count function and doc_freq_of_word function

--- Project/searchdata.py
+++ Project/searchdata.py
@@ -42,13 +42,47 @@ def get_page_rank(URL):
 #PREVIOUS CODE ABOVE COMPELETED BY SOPHIE
 import math
 
+#what documents we have:
+#outgoing.txt = file of outgoing links for each link
+#incoming.txt = file of incoming links for each link
+#pagerank.txt = file of ranks of each link?
+#readsites.txt = file of all files crawled thru + total link count
+# all N-X.txt = files of content of each link
+
+#get number of times word appears in a certain document
+def word_count(word, URL):
+    content = open(URL, 'r') #align formatting of URL w/ Sophie's formatting
+    words = content.strip().split(',')
+    word_count = 0
+
+    for each in words:
+        if each == f'{word}':
+            word_count += 1
+    
+    return word_count
+
+#get number of documents containing a specific word
+def doc_freq_of_word(word):
+    per_doc_count = 0
+    seed = open('readsites.txt', 'r')
+    total_links = int(seed.readline().strip())
+    all_links = seed.readlines()
+
+    for all in range(seed):
+        current = word_count(word, f'{(all_links[all].split('/'))[5].strip('.html\n')}.txt') #links to relevant text file containing relevant info
+        if current > 0:
+            per_doc_count += 1
+
+    seed.close()
+    return per_doc_count
+
+
 def get_idf(word):
 
-    if docFreq[word] == 0:
+    if doc_freq_of_word(word) == 0:
         return 0
     
-    #gotta really see what the previous code looks like before actually making more of this
-    #idfw = math.log((totalDocs/(1 + docFreq[word])), 2)
+    idfw = math.log((totalDocs/(1 + doc_freq_of_word(word))), 2)
 
     if idfw < 0:
         return 0
